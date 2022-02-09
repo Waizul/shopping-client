92c7c128da146969d6a5fa473628db04e5739786
Waizul Haque <waizul@gmail.com>
2022-02-09 13:34:02 +0600

    slider and category

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import NewsLetter from "../components/NewsLetter";
+import Products from "../components/Products";
 import Slider from "../components/Slider";
 
 
@@ -7,7 +10,11 @@ const Home = () => {
     return <div>
         <Announcement/>
         <Navbar/>
-        <Slider/>
+        <Slider />
+        <Categories />
+        <Products />
+        <NewsLetter/>
+        
     </div>;
 };
 
