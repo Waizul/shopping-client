2b06b7af6fd46583e4ced278ce24dfa92c439277
Waizul Haque <waizul@gmail.com>
2022-02-18 00:23:32 +0600

    /

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://shoppingbd.herokuapp.com/"
 
 const  TOKEN = localStorage.getItem('persist:root') ? JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser?.accessToken : ''
 
